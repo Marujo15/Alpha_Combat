1860b31deab1aca8425d00269e7288a7d11f9801
Augusto Varandas <augustovarandas21@gmail.com>
2024-11-03 00:32:14 -0300

    Feat: adds random background to pages

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -12,9 +12,6 @@ const LandingPage = () => {
             <Logo />
             <h1 className="title">Welcome to the Landing Page</h1>
             <Button className="login-btn" onClick={() => navigate('/login')}>Entrar</Button>
-            <Button className="register-btn" onClick={() => navigate('/register')}>Registrar</Button>
-            <Button className="rooms-page-btn" onClick={() => navigate('/rooms')}>Iniciar partida</Button>
-            <Button className="game-page-btn" onClick={() => navigate('/game')}>Iniciar partida</Button>
         </div>
     );
 };
