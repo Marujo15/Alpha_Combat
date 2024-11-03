f2143b19e8900a7c61ca9bbefdeba3960802e6a9
Augusto Varandas <augustovarandas21@gmail.com>
2024-11-03 00:59:14 -0300

    Feat: adjusts waiting page basic structure

--- frontend/src/pages/WaitingPage/WaitingPage.jsx
+++ frontend/src/pages/WaitingPage/WaitingPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from '../../components/Button/Button';
 import './WaitingPage.css';
 
 const WaitingPage = () => {
     return (
-        <div>
-            <h1>Welcome to the Waiting Room Page</h1>
+        <div className='waiting-page-container'>
+            {/* <div className='scoreboard-div'></div>
+            <div className='time-div'></div> */}
+            <div className='msg-div'>AGUARDANDO OUTRO PLAYER</div>
+            <Button className={'cancel-btn'} text='Back' onClick={() => {navigate('/dashboard')}}>CANCELAR</Button>
         </div>
     );
 };
