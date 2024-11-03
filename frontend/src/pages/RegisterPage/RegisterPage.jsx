98d8bf7059450ca0ad7793c3e42114a02e3814f3
Augusto Varandas <augustovarandas21@gmail.com>
2024-11-02 23:32:40 -0300

    Feat: adjusts register page basic structure

--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Logo from '../../components/Logo/Logo.jsx';
+import Form from '../../components/Form/Form';
+import Input from '../../components/Input/Input.jsx';
+import Button from '../../components/Button/Button.jsx';
 import './RegisterPage.css';
 
 const RegisterPage = () => {
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Form submitted');
+    };
+
     return (
         <div>
-            <h1>Welcome to the Register Page</h1>
+            <Logo />
+            <Form title={"CRIAR CONTA"} onSubmit={handleSubmit}>
+                <Input type="text" placeholder="APELIDO" />
+                <Input type="text" placeholder="EMAIL" />
+                <Input type="email" placeholder="SENHA" />
+                <div className="register-btns">
+                    <Button type="submit" className={"back-btn"}>VOLTAR</Button>
+                    <Button type="submit" className={"login-btn"}>ENTRAR</Button>
+                </div>
+            </Form>
         </div>
     );
 };
