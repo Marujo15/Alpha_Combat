e92545f17c2411f51a9c4c87e1ac989f6cadf9fe
Augusto Varandas <augustovarandas21@gmail.com>
2024-11-09 14:25:07 -0300

    Feature: Implements new functions to pages

--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
 import Logo from '../../components/Logo/Logo.jsx';
@@ -7,17 +7,66 @@ import Input from '../../components/Input/Input.jsx';
 import './DashboardPage.css';
 
 const DashboardPage = () => {
+    const { user } = useContext(UserContext);
+    const wsRef = useRef(null);
     const navigate = useNavigate();
-    const { user, logout } = useContext(UserContext);
+    const [gameStatus, setGameStatus] = useState("Connecting to server...");
+    const [messages, setMessages] = useState([]);
 
-    // useEffect(() => {
-    //     if (!user) {
-    //       navigate("/login");
-    //     }
+    useEffect(() => {
+        wsRef.current = new WebSocket('ws://localhost:3000');
 
-    //     console.log("User logged in:", user);
-    //   }
-    // , [user, navigate]);
+        wsRef.current.onopen = () => {
+            setGameStatus("Connected to server");
+        };
+
+        wsRef.current.onmessage = (event) => {
+            const receivedMessage = event.data;
+            const data = JSON.parse(event.data);
+
+            setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+
+            switch (data.type) {
+                case "roomCreated":
+                    handleUserRedirect(data.matchId);
+                    break;
+            }
+        };
+
+        wsRef.current.onclose = () => {
+            setGameStatus("Disconnected from server. Please, refresh the page to try again.");
+        };
+
+        wsRef.current.onerror = (error) => {
+            console.error("WebSocket error:", error);
+            setGameStatus("Error connecting to server.");
+        };
+
+        return () => {
+            if (wsRef.current) wsRef.current.close();
+        };
+    }, []);
+
+    const handleCreateRoom = () => {
+        if (wsRef.current.readyState === WebSocket.OPEN) {
+            const message = JSON.stringify({ type: "greeting", content: "Hello, server!" });
+            wsRef.current.send(message);
+        } else {
+            console.log("WebSocket is not open. Current state:", wsRef.current.readyState);
+        }
+
+        wsRef.current.send(
+            JSON.stringify({
+                type: "createNewRoom",
+                player1_id: user.user.id,
+                token: user.token,
+            })
+        );
+    };
+
+    const handleUserRedirect = (matchId) => {
+        navigate(`/game/${matchId}`);
+    };
 
     const handleStartGame = () => {
         console.log("Starting game...");
@@ -26,24 +75,14 @@ const DashboardPage = () => {
         // else, add player to waiting queue
     };
 
-    const handleLogout = () => {
-        logout();
-        localStorage.removeItem("token");
-        navigate("/login");
-      };
-
     return (
         <div className='dash-page-main-div'>
             <div className='dash-page-div'>
-                {/* <div className='find-game-div'> */}
-                    <p className='text'>ENCONTRAR JOGO</p>
-                    <Button type="submit" className={"create-room-btn"} onClick={() => {DashboardPage}}></Button>
-                {/* </div> */}
-                {/* <div className='code-name-div'> */}
-                    <p className='text'>OU INSIRA UM CÓDIGO:</p>
-                    <Input className="room-id-input" type="text" placeholder={"CÓDIGO DA SALA"} onChange={(e) => setRoomId(e.target.value)}></Input>
-                    <Button type="submit" className={"start-game-btn"} onClick={() => {handleStartGame}}></Button>
-                {/* </div> */}
+                <p className='text'>ENCONTRAR JOGO</p>
+                <Button type="submit" className={"create-room-btn"} onClick={handleCreateRoom}></Button>
+                <p className='text'>OU INSIRA UM CÓDIGO:</p>
+                <Input className="room-id-input" type="text" placeholder={"CÓDIGO DA SALA"} onChange={(e) => setRoomId(e.target.value)}></Input>
+                <Button type="submit" className={"start-game-btn"} onClick={() => {handleStartGame}}></Button>
                 <div>
                     <Button type="submit" className={"ranking-btn"} onClick={() => navigate('/rankings')}></Button>
                     <Button type="submit" className={"tutorial-btn"} onClick={() => navigate('/tutorial')}></Button>
--- frontend/src/pages/WaitingPage/WaitingPage.jsx
+++ frontend/src/pages/WaitingPage/WaitingPage.jsx
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef, useContext, useState } from 'react';
+import { UserContext } from '../../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
+import Input from '../../components/Input/Input';
 import './WaitingPage.css';
 
 const WaitingPage = () => {
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+    const wsRef = useRef(null);
+    const [gameStatus, setGameStatus] = useState("Connecting to server...");
+
+    useEffect(() => {
+        wsRef.current = new WebSocket('ws://localhost:3000');
+
+        wsRef.current.onopen = () => {
+            // console.log('Connected to WebSocket server');
+            setGameStatus("Connected to server");
+        };
+
+        wsRef.current.onmessage = (message) => {
+            const data = JSON.parse(message.data);
+            // console.log("Data received from server:", data);
+
+        };
+
+        wsRef.current.onclose = () => {
+            // console.log("Disconnected from server");
+            setGameStatus(
+                "Disconnected from server. Please, refresh the page to try again."
+            );
+        };
+
+        return () => {
+            if (wsRef.current) {
+                wsRef.current.close();
+            }
+        };
+    }, []);
+
+    const handleStartMatchBtn = () => {
+        console.log('Entering room...');
+
+        // this.ws.send(JSON.stringify({
+        //     type: 'enterRoom',
+        //     player: this.currentPlayer,
+        //     roomCode: this.roomCode,
+        // }));
+    };
+
     return (
         <div className='waiting-page-container'>
             <div className='waiting-page-div'>
@@ -12,7 +58,7 @@ const WaitingPage = () => {
                 </div>
                 <div className='waiting-buttons-div'>
                     <Button type="submit" className={"leave-btn"} onClick={() => navigate('/dashboard')}></Button>
-                    <Button type="submit" className={"start-match-btn"} onClick={() => {}}></Button>
+                    <Button type="submit" className={"start-match-btn"} onClick={handleStartMatchBtn}></Button>
                     <Button type="submit" className={"ready-btn"} onClick={() => {}}></Button>
                 </div>
             </div>
