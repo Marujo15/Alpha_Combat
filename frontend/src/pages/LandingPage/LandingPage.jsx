23b592fad27c9eb5e22df847db39ebd7838618d6
murilobarbosaa <murilobarbosa2811@gmail.com>
2024-11-11 23:12:31 -0300

    Fix: fixing backgrounds and some details

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -9,13 +9,21 @@ const LandingPage = () => {
     return (
         <div className="landing-page-container">
             <div>
-                {/* <h1 className="title">ALPHA COMBAT</h1> */}
+                <h1 className="title">ALPHA COMBAT</h1>
             </div>
             <div>
                 <Button className="go-to-login-btn" onClick={() => navigate('/login')}></Button>
             </div>
             <div className='about'>
-                <p>PREPARE-SE PARA A ADRENALINA DE ALPHA COMBAT! EM UM DUELO MULTIPLAYER ELETRIZANTE, VOCÊ ASSUME O CONTROLE DE UM TANQUE DE GUERRA COM UM ÚNICO OBJETIVO: DESTRUIR O INIMIGO ANTES QUE ELE DESTRUA VOCÊ. COM GRÁFICOS IMERSIVOS E MAPAS REPLETOS DE OBSTÁCULOS ESTRATÉGICOS, CADA PARTIDA EXIGE HABILIDADE, PRECISÃO E UMA BOA DOSE DE ESTRATÉGIA. SÓ OS MELHORES SOBREVIVEM EM ALPHA COMBAT!</p>
+                <div className='flag'>
+                    <img className='images' src="../../../assets/flag.png" alt="flag" />
+                </div>
+                <div>
+                    <p>PREPARE-SE PARA A ADRENALINA DE ALPHA COMBAT! EM UM DUELO MULTIPLAYER ELETRIZANTE, VOCÊ ASSUME O CONTROLE DE UM TANQUE DE GUERRA COM UM ÚNICO OBJETIVO: DESTRUIR O INIMIGO ANTES QUE ELE DESTRUA VOCÊ. COM GRÁFICOS IMERSIVOS E MAPAS REPLETOS DE OBSTÁCULOS ESTRATÉGICOS, CADA PARTIDA EXIGE HABILIDADE, PRECISÃO E UMA BOA DOSE DE ESTRATÉGIA. SÓ OS MELHORES SOBREVIVEM EM ALPHA COMBAT!</p>
+                </div>
+                <div className='trophy'>
+                    <img className='images' src="../../../assets/trophy.png" alt="trophy" />
+                </div> 
             </div>
         </div>
     );
