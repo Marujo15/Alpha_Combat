c44af78b79cce38234f4542cff9e41bb986eefe0
Marujo <luiz.marujo15@gmail.com>
2024-11-18 14:58:47 -0300

    temp

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import server from "./app.js"
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
